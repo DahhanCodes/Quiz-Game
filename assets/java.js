27d0cf413567d884bde8bbdff3055ffb303c3f43
Dahhan_coder <moustafaeldahhan@gmail.com>
2022-01-04 21:37:04 -0500

    new files

--- assets/java.js
+++ assets/java.js
@@ -11,55 +11,127 @@ var choices = ["strings", "booleans", "alerts", "numbers", "quotes", "curly brac
                "parenthesis", "Javascript", "terminal / bash", "for loops", "console log"];
 var correct = ["alerts", "parentheses", "all of the above","quotes","console log" ];
 var score= 0;
-var time = 0; 
-var element;
+var time = 5; 
+var timeLeft;
+var penalty= 1;
+var questionNum=0;
+var names=[];
+var scoresList= [];
+
+
 //creating a button variable to access the html button
 var button = document.getElementById("button");
+var highScores = document.getElementById("highScores")
 //adding an event listener to check if user clicked the generate password button
+highScores.addEventListener('click',scores)
+button.addEventListener('click', timer);
 button.addEventListener('click', activated);
 //creating a function that will help create a random password
-function activated(event){
-    var introPage = document.getElementById("intro");
-    var questionPage= document.getElementById("questionBox");
-   
+var introPage = document.getElementById("intro");
+var questionPage= document.getElementById("questionBox");
+function scores(){
     introPage.style.display= "none";
+    questionPage.style.display= "none";
+    var list = $("<li>").addClass("listOfScores")
+    for(let i=0;i<names.length(); i++)
+    $("#scorePage").append(list).text(names[i]+" : "+scores[i]);
+    document.write("\n");
+}
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+  async function timer(){
+      for (let i=0; i<5 ; i++){
+    await sleep(60000)
+    time -= 1;
+    document.getElementById("time").innerHTML = time;
+  }
+}
+function activated(event){
+    introPage.style.display= "none";
+    document.getElementById("time").innerHTML = time;
     document.getElementById("question").innerHTML = questions[0];
     document.getElementById("ansOne").innerHTML =  choices[0];
     document.getElementById("ansTwo").innerHTML = choices[1];
     document.getElementById("ansThree").innerHTML = choices[2];
     document.getElementById("ansFour").innerHTML = choices[3]
+    
         questionPage.style.display= "block";
         
 }
-function quiz(event){ 
-    var element = event.target;
+$(".answers").on("click","li",async function check(){
+    var correctMessage= $("<p>").addClass("message").text("Correct Answer!")
+    var wrongMessage= $("<p>").addClass("message").text("Wrong! The correct answer is:  " + correct[questionNum]);
+    var answer=$(this).text().trim();
+    String(answer);
+    console.log(answer)
+    console.log(correct[questionNum])
     var createDiv = document.createElement("div");
         createDiv.setAttribute("id", "createDiv");
     for(let i = 0; i < correct.length; i++ ){
-        if (element.textContent == correct[i]) {
+        if (answer == correct[questionNum]) {
             score++;
-            createDiv.textContent = "Correct! The answer is:  " + correct[i];
-            // Correct condition 
-        } else {
+            $("#questionBox").append(correctMessage);
+            // Correct condition
+            
+            break;
+        } 
+    }
+    for(let i = 0; i < correct.length; i++ ){
+        if (answer != correct[questionNum]) {
             // Will deduct -5 seconds off secondsLeft for wrong answers
-            secondsLeft = secondsLeft - penalty;
-            createDiv.textContent = "Wrong! The correct answer is:  " + correct[i];
+            time = time - penalty;
+            $("#questionBox").append(wrongMessage);
+            
+             break;
         }
+    }
 
 
-    }    
-
+    
+    
+    questionNum ++;
     questions.shift();
     choices.splice(0,4);
     console.log(choices)
     console.log(questions)
-    if(questions===undefined && choices===undefined){
+    if(questions[0]===undefined || time==0){
         var done = document.getElementById("end");
         questionPage.style.display= "none";
         done.style.display="block";
+        document.getElementById("score").innerHTML = score;
+        $("#name").on("click","input",function(){
+            var text =$(this).text().trim();
+            console.log(text);
+           //create a new <textarea> element
+           var textInput= $("<textarea>").val(text);
+            $(this).replaceWith(textInput);
+             textInput.trigger("focus");
+             console.log(textInput);
+           
+          });
+          
+          //name field is un-focused
+          $("#name").on("blur","textarea",function(){
+            //get the textarea value
+            var text= $(this).val().trim();
+            names.push(text);
+            String(score);
+            scoresList.push(score)
+           
+          
+          })
     }
     else{
-    activated();     
+        
+   await sleep(2000);
+   $(".message").css("display","none");
+   activated(); 
+     
     }
-}
+       
+    
+});
 
